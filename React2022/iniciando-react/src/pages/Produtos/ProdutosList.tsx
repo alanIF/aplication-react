f19506974cee6e4dfc655d937ec3dcdf32cd2854
Alan Tavares <alan_tj@hotmail.com.br>
2022-02-06 14:37:15 -0300

    listprodutos

--- React2022/iniciando-react/src/pages/Produtos/ProdutosList.tsx
+++ React2022/iniciando-react/src/pages/Produtos/ProdutosList.tsx
@@ -1,3 +1,44 @@
-export function ProdutosList(){
-    return <h1>Teste</h1>
+import {useState,useEffect} from 'react';
+
+import Axios from "axios";
+
+
+const  ProdutosList= () =>{
+    const [listProdutos, setListProdutos] = useState([]);
+    
+   // listar dados
+    useEffect(() => {
+        Axios.get("https://localhost:7252/api/Produto/index").then((response) => {
+            setListProdutos(response.data);
+        });
+    }, []);
+    return(
+        
+       <div>
+           <table className="table table-responsive table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+
+                        <th>Nome</th>
+                        <th>Estoque Mínimo</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                {listProdutos.map((val) => (
+        <td> {val.Id}   </td>
+        <td> {val.Nome}   </td>
+        <td> {val.EstoqueMinimo}   </td>
+
+        ))
+        }
+                </tbody>
+           </table>
+              
+               
+           
+       </div>
+    );    
 }
+export default ProdutosList;
